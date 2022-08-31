8ae28bce7b18b941634d11e200440234569fc15b
Rosemary <rjingrey@gmail.com>
2022-09-01 08:18:29 +1000

    changed javascript to add and remove classes instead with transition

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 document.querySelectorAll('.box').forEach(box => {
 	box.addEventListener('mouseover', event => {
 
-		box.style.background = '#FFFFFF';
+		box.classList.add('on');
+		box.classList.remove('off');
+
+	})
+})
+
+document.querySelectorAll('.box').forEach(box => {
+	box.addEventListener('mouseout', event => {
+
+		box.classList.remove('on');
+		box.classList.add('off');
 
 	})
 })
