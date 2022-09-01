8ae3fd07e08979c2bcb2570f92a413b045b0ea98
Rosemary <rjingrey@gmail.com>
2022-09-01 17:00:21 +1000

    divs created with function instead of hardcoded in html

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function createGrid(num) {
+	num = num * num;
+	const container = document.querySelector('.container');
+
+	for (x = 0; x < num; x++) {
+		const box = document.createElement('div');
+		box.classList.add('box');
+		container.appendChild(box);
+	}
+}
+
+createGrid(4);
+
 document.querySelectorAll('.box').forEach(box => {
 	box.addEventListener('mouseover', event => {
 
